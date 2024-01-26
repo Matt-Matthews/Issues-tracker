5c80a170db951392f9e64f33fba6b15b72df871c
Mathews Paku <mathewspaku@gmail.com>
2024-01-26 15:08:44 +0200

    Refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
-import {z} from 'zod';
+import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 
@@ -16,31 +16,36 @@ type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, formState: {errors} } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsSubmitted(true);
+      await axios.post("/api/issues", data);
+      router.replace("/issues");
+    } catch (error) {
+      setIsSubmitted(false);
+      setError("An unexpected error occurred.");
+    }
+  });
+
   return (
     <div className="max-w-xl space-y-5">
-      {error&&<Callout.Root color="red">
-        <Callout.Text>{error}</Callout.Text>
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
-      }
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setIsSubmitted(true);
-            await axios.post("/api/issues", data);
-            router.replace("/issues");
-          } catch (error) {
-            setIsSubmitted(false);
-            setError("An unexpected error occurred.");
-          }
-        })}
-      >
+      )}
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
@@ -53,7 +58,9 @@ const NewIssuePage = () => {
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled={isSubmitted}>Submit New Issue {isSubmitted&&<Spinner />}</Button>
+        <Button disabled={isSubmitted}>
+          Submit New Issue {isSubmitted && <Spinner />}
+        </Button>
       </form>
     </div>
   );
